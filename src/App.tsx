ec9f55a0aa19f77514156637708cb1b8af1d8405
emmaknaub <emma.knaub27@gmail.com>
2024-01-09 10:55:30 -0500

    iphone pro max

--- src/App.tsx
+++ src/App.tsx
@@ -8,4 +8,30 @@ function App() {
 	);
 }
 
+export const IphoneProMax = (): JSX.Element => {
+	return (
+	  <div className="iphone-pro-max">
+		<div className="overlap">
+		  <div className="text-wrapper">DESSERTS</div>
+		</div>
+		<div className="overlap-group">
+		  <img className="COVER-IMAGE" alt="Cover IMAGE" src="COVER-IMAGE.png" />
+		  <div className="div">Unicorn Sparkle Cupcakes 🦄✨</div>
+		  <p className="p">
+			Effervescent frosting imbued with tart unicorn tears and expired rainbows, piped delicately upon a delectable
+			confectionary surprise!
+		  </p>
+		  <div className="div-wrapper">
+			<div className="text-wrapper-2">ORDER A DOZEN</div>
+		  </div>
+		</div>
+		<Logo className="logo-instance" />
+		<div className="text-wrapper-3">SOUPS</div>
+		<div className="text-wrapper-4">PET FOOD</div>
+		<img className="PERSON" alt="Person" src="PERSON.png" />
+		<img className="CART" alt="Cart" src="CART.png" />
+	  </div>
+	);
+  };
+
 export default App;
